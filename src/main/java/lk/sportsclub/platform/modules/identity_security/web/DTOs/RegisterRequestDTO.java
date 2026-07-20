304d083a66c9cda1a657dca5aba8cb81365cfcbf
Yahan-Botheju <yahan.ravinga.git@gmail.com>
2026-07-20 21:51:14 +0530

    create web layer classes

---
+++ src/main/java/lk/sportsclub/platform/modules/identity_security/web/DTOs/RegisterRequestDTO.java
@@ -0,0 +1,5 @@
+package lk.sportsclub.platform.modules.identity_security.web.DTOs;
+
+public class RegisterRequestDTO
+{
+}
---
+++ src/main/java/lk/sportsclub/platform/modules/identity_security/web/webMapper/AuthWebMapper.java
@@ -0,0 +1,4 @@
+package lk.sportsclub.platform.modules.identity_security.web.webMapper;
+
+public interface AuthWebMapper {
+}
