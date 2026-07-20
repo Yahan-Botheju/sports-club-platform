b98357b0b00e6477fbe06e76b14904cae1c14ea2
Yahan-Botheju <yahan.ravinga.git@gmail.com>
2026-07-20 21:32:39 +0530

    create config classes

---
+++ src/main/java/lk/sportsclub/platform/modules/identity_security/infrastructure/persistence/UserRepositoryImpl.java
@@ -0,0 +1,4 @@
+package lk.sportsclub.platform.modules.identity_security.infrastructure.persistence;
+
+public class UserRepositoryImpl {
+}
---
+++ src/main/java/lk/sportsclub/platform/modules/identity_security/infrastructure/persistence/entities/UserEntity.java
@@ -0,0 +1,4 @@
+package lk.sportsclub.platform.modules.identity_security.infrastructure.persistence.entities;
+
+public class UserEntity {
+}
