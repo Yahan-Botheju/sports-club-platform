2023ef94507ccc0acc75db5c18f7c1a79a74742d
Yahan-Botheju <yahan.ravinga.git@gmail.com>
2026-07-21 22:20:41 +0530

    feat: initiate resource not found exception error  functionality

--- src/main/java/lk/sportsclub/platform/shared/exceptions/GlobalExceptionHandler.java
+++ src/main/java/lk/sportsclub/platform/shared/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package lk.sportsclub.platform.shared.exceptions;
 
+import lk.sportsclub.platform.shared.DTOs.ErrorDetail;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNotFound(ResourceNotFoundException ex){
+        ErrorDetail error = new ErrorDetail("NOT_FOUND", ex.getMessage(), LocalDateTime.now())
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error(error));
+    }
+
 }
