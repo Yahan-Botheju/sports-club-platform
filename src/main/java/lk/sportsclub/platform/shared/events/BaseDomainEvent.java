fb06b51401ad1cbc46ac1fac76b67f788ae4f02c
Yahan-Botheju <yahan.ravinga.git@gmail.com>
2026-07-22 23:31:15 +0530

    feat: create base domain class for domain event

--- src/main/java/lk/sportsclub/platform/shared/events/BaseDomainEvent.java
+++ src/main/java/lk/sportsclub/platform/shared/events/BaseDomainEvent.java
@@ -1,4 +1,15 @@
 package lk.sportsclub.platform.shared.events;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public abstract class BaseDomainEvent {
+    private final LocalDateTime occurredOn;
+
+
+    protected BaseDomainEvent() {
+        this.occurredOn = LocalDateTime.now();
+    }
 }
