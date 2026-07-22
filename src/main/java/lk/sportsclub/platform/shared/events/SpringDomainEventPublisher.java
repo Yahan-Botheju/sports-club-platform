6b41fb773692429bdac7aca37ea832c27a9adf85
Yahan-Botheju <yahan.ravinga.git@gmail.com>
2026-07-22 23:38:38 +0530

    feat: initiate event publisher method

--- src/main/java/lk/sportsclub/platform/shared/events/SpringDomainEventPublisher.java
+++ src/main/java/lk/sportsclub/platform/shared/events/SpringDomainEventPublisher.java
@@ -6,5 +6,12 @@ public class SpringDomainEventPublisher implements DomainEventPublisher{
     //inject required dependencies
     private final ApplicationEventPublisher applicationEventPublisher;
 
+    public SpringDomainEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
 
+    @Override
+    public void publish(BaseDomainEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
 }
